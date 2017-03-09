cefc527bf1a2ee2effe71319d53b17149bc9eac6
luheyun <akheyun@gmail.com>
2017-03-09 20:16:24 +0800

    update

--- Assets/Scripts/NativeParticleSystem.cs
+++ Assets/Scripts/NativeParticleSystem.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NativeParticleSystem : MonoBehaviour {
+public class NativeParticleSystem : MonoBehaviour
+{
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+	    
 	}
 }
